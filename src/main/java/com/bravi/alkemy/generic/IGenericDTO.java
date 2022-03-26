15d24a29db809549a827665d48358a8a580905b9
lucasbravi2019 <lucas.bravi.2008@gmail.com>
2022-03-26 10:35:51 -0300

    Aceleracion Alkemy Lucas Bravi

--- src/main/java/com/bravi/alkemy/generic/IGenericDTO.java
+++ src/main/java/com/bravi/alkemy/generic/IGenericDTO.java
@@ -1,5 +1,8 @@
 package com.bravi.alkemy.generic;
 
+/**
+ * DTO interface used in every DTO as a Factory for data representation of different types of Records
+ */
 public interface IGenericDTO {
 
     public Record generateRecord(RecordType recordType);
--- src/main/java/com/bravi/alkemy/generic/IGenericMapper.java
+++ src/main/java/com/bravi/alkemy/generic/IGenericMapper.java
@@ -2,6 +2,11 @@ package com.bravi.alkemy.generic;
 
 import java.util.List;
 
+/**
+ * Interface used in every Mapper to transform Entities into DTO objects, and DTO objects into Entities
+ * @param <E> Entity used
+ * @param <T> DTO subclass of IGenericDTO used to map the entity
+ */
 public interface IGenericMapper<E, T extends IGenericDTO> {
 
     public E toEntity(T dto);
